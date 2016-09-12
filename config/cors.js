7578fc15035d5a4b39a792ff42ac03f46b102192
Bruno Santos <brunosaantos00@gmail.com>
2016-09-12 15:15:01 -0300

    adding x-access-token to the list of allowed request header

--- config/cors.js
+++ config/cors.js
@@ -1,14 +1,13 @@
 export default (app) => {
   app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Access-Token');
     return next();
   });
   //
   function corsHandler(req, res, next) {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Cache-Control, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-Access-Token, X-PINGOTHER, X-CSRF-Token');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Cache-Control, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-Access-Token, X-PINGOTHER, X-CSRF-Token');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
